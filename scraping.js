f685b85f8424d766feffe596d7ce6e73a3389097
Genisson Emilio <esgenisson@gmail.com>
2025-02-28 19:26:22 -0300

    update
    atualizando script

--- scraping.js
+++ scraping.js
@@ -7,17 +7,17 @@ try {
         const page = await browser.newPage();
     
         //Acessa a pagina de login da steam
-        await page.goto('https://store.steampowered.com/login/');
+        await page.goto('https://secure.nuuvem.com/br-pt/account/login?_gl=1*pi1ci0*_gcl_au*MzQ5NzYwMzAyLjE3MzUzODcwMzc.*_ga*OTg1NDYwMDUxLjE3MzUzODcwMzc.*_ga_3BQRZREB88*MTc0MDc4MDk0NC41LjEuMTc0MDc4MDk1My41MS4wLjA.');
         console.log('Faça login manualmente e pressione Enter quando estiver pronto.')
         process.stdin.once('data', async () => {
             //Pagina de historico de compras
-            await page.goto('https://store.steampowered.com/account/history/');
-            await page.waitForSelector('.wht_total', { timeout: 60000 });
+            await page.goto('https://secure.nuuvem.com/br-pt/account/orders');
+            await page.waitForSelector('.store-orders-item-header-col-subtitle', { timeout: 6000 });
     
             //Extrair os valores de gastos
             const totalSpent = await page.evaluate(() => {
                 let total = 0;
-                const elements = document.querySelectorAll('.wht_total');
+                const elements = document.querySelectorAll('.store-orders-item-header-col-subtitle');
             
                 if (elements.length === 0) {
                     return 'Erro: Nenhum valor encontrado';
@@ -42,7 +42,7 @@ try {
             };
     
             console.log(`Total gasto: R$ ${totalSpent}`)
-            fs.writeFileSync('dadosSteam.json', JSON.stringify(dados, null, 2), 'utf-8');
+            fs.writeFileSync('dadosNuvem.json', JSON.stringify(dados, null, 2), 'utf-8');
             console.log('Arquivos salvos com sucesso');
     
             await browser.close();

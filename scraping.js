91312a986a762764c8ada292bace46961fa8e86f
Genisson Emilio <esgenisson@gmail.com>
2025-03-26 19:16:37 -0300

    Update scraping.js

--- scraping.js
+++ scraping.js
@@ -7,12 +7,12 @@ try {
         const page = await browser.newPage();
     
         //Acessa a pagina de login da steam
-        await page.goto('https://secure.nuuvem.com/br-pt/account/login?_gl=1*pi1ci0*_gcl_au*MzQ5NzYwMzAyLjE3MzUzODcwMzc.*_ga*OTg1NDYwMDUxLjE3MzUzODcwMzc.*_ga_3BQRZREB88*MTc0MDc4MDk0NC41LjEuMTc0MDc4MDk1My41MS4wLjA.');
+        await page.goto('https://secure.nuuvem.com/br-pt/account/login');
         console.log('Faça login manualmente e pressione Enter quando estiver pronto.')
         process.stdin.once('data', async () => {
             //Pagina de historico de compras
             await page.goto('https://secure.nuuvem.com/br-pt/account/orders');
-            await page.waitForSelector('.store-orders-item-header-col-subtitle', { timeout: 6000 });
+            await page.waitForSelector('.=store-orders-item-header-col-subtitle', { timeout: 6000 });
     
             //Extrair os valores de gastos
             const totalSpent = await page.evaluate(() => {
